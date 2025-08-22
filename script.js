24832ca799074a4f44566a6a33251742d4b1500f
Luiz <gustavopegonobre@gmail.com>
2025-08-21 22:21:22 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const messages = [
+  "Olá, meu nome é Luiz",
+  "Sou desenvolvedor full stack",
+];
+
+const typingElement = document.getElementById("typing-text");
+let messageIndex = 0, charIndex = 0, isDeleting = false;
+
+function typeEffect() {
+  const currentMessage = messages[messageIndex];
+  if (isDeleting) {
+    typingElement.textContent = currentMessage.substring(0, charIndex - 1);
+    charIndex--;
+  } else {
+    typingElement.textContent = currentMessage.substring(0, charIndex + 1);
+    charIndex++;
+  }
+  let typingSpeed = isDeleting ? 60 : 100;
+  if (!isDeleting && charIndex === currentMessage.length) {
+    typingSpeed = 1500;
+    isDeleting = true;
+  } else if (isDeleting && charIndex === 0) {
+    isDeleting = false;
+    messageIndex = (messageIndex + 1) % messages.length;
+    typingSpeed = 300;
+  }
+  setTimeout(typeEffect, typingSpeed);
+}
+
+typeEffect();
